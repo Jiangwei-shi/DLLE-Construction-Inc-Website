37280dbce42be56fead57c7521bedbc2607f21dd
Jiangwei-shi <a1006351432@gmail.com>
2024-12-05 14:39:29 -0500

    finished header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Header from "../components/Header/Header";
+import React from "react";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+      <>
+        <Header />
+      </>
+  );
 }
---
+++ app/welcome/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// @ts-ignore
+import Header from '../../components/Header/Header';
+
+export default function HomePage() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
