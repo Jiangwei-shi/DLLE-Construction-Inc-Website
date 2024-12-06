694a9773b4adc1ecbe557c9f35e6d11fec9acd66
Jiangwei-shi <a1006351432@gmail.com>
2024-12-06 15:20:01 -0500

    upload video and put in the page

--- components/MainPage/MainPage.tsx
+++ components/MainPage/MainPage.tsx
@@ -1,7 +1,8 @@
 'use client';
 
-import {SimpleGrid} from "@mantine/core";
+import {SimpleGrid, Text} from "@mantine/core";
 import Item from "./Item";
+import classes from './MainPage.module.css';
 
 
 export default function MainPage(){
@@ -10,63 +11,75 @@ export default function MainPage(){
     const pictureUrl3 = "/3.jpg"
     const pictureUrl4 = "/4.jpg"
     const pictureUrl5 = "/5.jpg"
+    const videoUrl = "/palette.mp4"
     return (
-    <SimpleGrid cols={1} spacing="0" verticalSpacing="0">
-        <Item
-            title="Start your decorating journey"
-            desc="desc"
-            descLink=""
-            backgroundImg={pictureUrl2}
-            leftBtnText="Order now"
-            leftBtnLink=""
-            rightBtnText="Learn more"
-            rightBtnLink=""
-            twoButtons={true}
-        />
-        <Item
-            title="Create Your Dream Space Today"
-            desc="desc"
-            descLink=""
-            backgroundImg={pictureUrl1}
-            leftBtnText="left"
-            leftBtnLink=""
-            rightBtnText="right"
-            rightBtnLink=""
-            twoButtons={true}
-        />
-        <Item
-            title="Shape Your Sanctuary Today"
-            desc="desc"
-            descLink=""
-            backgroundImg={pictureUrl3}
-            leftBtnText="left"
-            leftBtnLink=""
-            rightBtnText="right"
-            rightBtnLink=""
-            twoButtons={true}
-        />
-        <Item
-            title="Live in Beauty, Live in Style"
-            desc="desc"
-            descLink=""
-            backgroundImg={pictureUrl4}
-            leftBtnText="left"
-            leftBtnLink=""
-            rightBtnText="right"
-            rightBtnLink=""
-            twoButtons={true}
-        />
-        <Item
-            title="Reimagine city, Redefine Comfort"
-            desc="desc"
-            descLink=""
-            backgroundImg={pictureUrl5}
-            leftBtnText="left"
-            leftBtnLink=""
-            rightBtnText="right"
-            rightBtnLink=""
-            twoButtons={true}
-        />
-    </SimpleGrid>
+        <SimpleGrid cols={1} spacing="0" verticalSpacing="0">
+            <Item
+                title="Start your decorating journey"
+                desc="desc"
+                descLink=""
+                backgroundImg={pictureUrl2}
+                leftBtnText="Order now"
+                leftBtnLink=""
+                rightBtnText="Learn more"
+                rightBtnLink=""
+                twoButtons={true}
+            />
+            <Item
+                title="Create Your Dream Space Today"
+                desc="desc"
+                descLink=""
+                backgroundImg={pictureUrl1}
+                leftBtnText="left"
+                leftBtnLink=""
+                rightBtnText="right"
+                rightBtnLink=""
+                twoButtons={true}
+            />
+            <Item
+                title="Shape Your Sanctuary Today"
+                desc="desc"
+                descLink=""
+                backgroundImg={pictureUrl3}
+                leftBtnText="left"
+                leftBtnLink=""
+                rightBtnText="right"
+                rightBtnLink=""
+                twoButtons={true}
+            />
+            <Item
+                title="Live in Beauty, Live in Style"
+                desc="desc"
+                descLink=""
+                backgroundImg={pictureUrl4}
+                leftBtnText="left"
+                leftBtnLink=""
+                rightBtnText="right"
+                rightBtnLink=""
+                twoButtons={true}
+            />
+            <Item
+                title="Reimagine city, Redefine Comfort"
+                desc="desc"
+                descLink=""
+                backgroundImg={pictureUrl5}
+                leftBtnText="left"
+                leftBtnLink=""
+                rightBtnText="right"
+                rightBtnLink=""
+                twoButtons={true}
+            />
+            <div className={classes.backgroundContainer}>
+                <video className={classes.backgroundVideo} autoPlay loop muted>
+                    <source src={videoUrl} type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+                <div className="content-overlay">
+                    <Text className={classes.title}>
+                        开始装修
+                    </Text>
+                </div>
+            </div>
+        </SimpleGrid>
     );
 }
